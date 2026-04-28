29dcd502cbe25f9250d2fab726cb1bcdb102995c
loranmatos68 <loranmatos68@gmail.com>
2026-04-27 21:59:05 -0300

    Add files via upload

---
+++ Atividade Prática Estruturas de Repetição (while e for) em Python.py
@@ -0,0 +1,8 @@
+num = int (input("Digite um valor para a tabuada "))
+print("Tabuada")
+i = 1
+while i <= 10:
+    res = num * i
+    print(num," x ", i," = ", res)
+    i += 1
+print("Fim da Tabuada")
